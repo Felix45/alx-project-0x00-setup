372308870ab1f0c33dbcc41f5739161650cf7f33
felix45 <fatonoh@gmail.com>
2025-06-01 00:42:56 +0300

    Set up landing page and homepage

---
+++ pages/landing.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "@/components/Card";
+import Button from "@/components/Button";
+
+const Landing: React.FC = () => {
+    return (
+        <div className="container">
+            <h1 className=" text-xl font-extralight">Landing Page</h1>
+            <div className="flex">
+              <Card />
+              <Card />
+              <Card />
+            </div>
+            <div className="flex">
+                <Button title="Book Now" styles="btn-sm rounded-sm py-2 px-4 cursor-pointer font-bold bg-green-500" />
+                <Button title="Cancel" styles="btn-sm rounded-md py-3  px-4 cursor-pointer font-bold bg-green-500 ml-2" />
+                <Button title="Register" styles="btn-sm rounded-full p-4 cursor-pointer font-bold bg-green-500 ml-2" />
+            </div>
+            <div className="flex mt-3">
+                <Button title="Book Now" styles="btn-sm rounded-sm py-2 px-4 cursor-pointer font-bold" />
+                <Button title="Cancel" styles="btn-sm rounded-md py-3  px-4 cursor-pointer font-bold ml-2" />
+                <Button title="Register" styles="btn-sm rounded-full p-4 cursor-pointer font-bold ml-2" />
+            </div>
+
+        </div>
+    );
+}
+
+export default Landing;
